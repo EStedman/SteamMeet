1355f4dac5e97738317f673b7aa9ced26a629936
ShinigamiRogue <stedmane@mail.gvsu.edu>
2014-04-18 05:04:29 -0400

    Switched spinner for a button

--- SteamMeet/src/edu/gvsu/cis/greenmbr/stedmane/SteamMeet/EventsMain.java
+++ SteamMeet/src/edu/gvsu/cis/greenmbr/stedmane/SteamMeet/EventsMain.java
@@ -1,7 +1,18 @@
 package edu.gvsu.cis.greenmbr.stedmane.SteamMeet;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * Created by Evan on 4/18/14.
  */
-public class EventsMain {
+public class EventsMain extends Activity{
+    private TextView hey;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_page);
+        hey = (TextView) findViewById(R.id.hey);
+    }
 }
--- SteamMeet/src/edu/gvsu/cis/greenmbr/stedmane/SteamMeet/MyActivity.java
+++ SteamMeet/src/edu/gvsu/cis/greenmbr/stedmane/SteamMeet/MyActivity.java
@@ -5,9 +5,8 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,13 +16,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements View.OnClickListener {
     // Called when the activity is first created.
     private TextView profile, user, clanID, state;
     private String userSave, profileSave, clanSave, stateSave;
     private ImageView avatar;
     private String profNumber;
-    private Spinner eventOrProfile;
+    private Button eventButton;
+    //private Spinner eventOrProfile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,9 +34,21 @@ public class MyActivity extends Activity {
         state = (TextView) findViewById(R.id.activeState);
         clanID = (TextView) findViewById(R.id.clan);
         avatar = (ImageView) findViewById(R.id.imageView);
-        //eventOrProfile = (Spinner)
+        eventButton = (Button) findViewById(R.id.toEvents);
         Intent intented = getIntent();
         profNumber = intented.getStringExtra("storage");
+        eventButton.setOnClickListener(this);
+
+        /*If have time to finish spinner
+
+        eventOrProfile = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_items, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventOrProfile.setAdapter(adapter);
+        eventOrProfile.setOnItemSelectedListener(this);
+        */
+
         if(savedInstanceState != null){
             userSave = savedInstanceState.getString("UserSave");
             profileSave = savedInstanceState.getString("ProfileSave");
@@ -48,8 +60,9 @@ public class MyActivity extends Activity {
             state.setText(stateSave);
             new imageTask().execute();
         }
-        else
+        else{
             new ProfileTask().execute();
+        }
     }
 
     @Override
@@ -64,6 +77,36 @@ public class MyActivity extends Activity {
         outState.putString("ClanSave", clanID.getText().toString());
         outState.putString("StateSave", state.getText().toString());
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v == eventButton){
+            Intent toEvents = new Intent(this, EventsMain.class);
+            startActivity(toEvents);
+        }
+    }
+
+    /*Part of spinner
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        String selectedItem = parent.getItemAtPosition(pos).toString();
+        if("Events".equalsIgnoreCase(selectedItem)){
+            Intent eventPass = new Intent(this, EventsMain.class);
+            startActivity(eventPass);
+        }
+        if("Profile".equalsIgnoreCase(selectedItem)){
+            Intent profilePass = new Intent(this, MyActivity.class);
+            startActivity(profilePass);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        setContentView(R.layout.profileview);
+    }
+    */
+
     private class ProfileTask extends AsyncTask<Void, Integer, JSONObject> {
         private Drawable avatarImg;
         @Override
@@ -73,7 +116,7 @@ public class MyActivity extends Activity {
                 profileURL = new URL("http://api.steampowered.com/" +
                         "ISteamUser/GetPlayerSummaries/v00" +
                         "02/?key=A35259FADACBD1E99D1101AD8" +
-                        "4321147&steamids=" + profNumber);
+                        "4321147&steamids=" + "76561198046688891");//profNumber
                 String out = "";
                 HttpURLConnection conn = (HttpURLConnection) profileURL.openConnection();
                 Scanner scan = new Scanner(conn.getInputStream());
