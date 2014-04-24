285878df960370a7ec17ea97c986b9f5138e01f6
RenegadeBanana <greenmanbrett@gmail.com>
2014-04-23 21:36:39 -0400

    Reconciling deep-seated differences

--- SteamMeet/src/edu/gvsu/cis/greenmbr/stedmane/SteamMeet/CreatePlaces.java
+++ SteamMeet/src/edu/gvsu/cis/greenmbr/stedmane/SteamMeet/CreatePlaces.java
@@ -1,11 +1,19 @@
 package edu.gvsu.cis.greenmbr.stedmane.SteamMeet;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.widget.EditText;
 
 /**
  * Created by Brett on 4/18/14.
  */
 public class CreatePlaces extends Activity {
-   // EditText
+    /*
+    LocationManager locationManager = (LocationManager)
+            getSystemService(Context.LOCATION_SERVICE);
+    LocationListener locationListener = new MyLocationListener();
+    locationManager.requestLocationUpdates(
+    LocationManager.GPS_PROVIDER, 5000, 10, locationListener); */
 }
